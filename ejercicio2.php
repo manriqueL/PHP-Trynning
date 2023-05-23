04b821520bf9e5847a91e498b9e252d8980167e4
manriqueL <manriquelucasp@gmail.com>
2023-05-23 12:32:12 -0300

    Se modifica la forma de mostrar el array cargado con los 120 elementos

--- ejercicio2.php
+++ ejercicio2.php
@@ -4,19 +4,13 @@ $arreglo = [];
 
 while (sizeof($arreglo) < 120) {
     array_push($arreglo, random_int(0,1));
+    
 };
 
-//Funcion para mostrar el arreglo
-function mostrarArreglo($arreglo){
-    $resultado = "";
-    foreach ($arreglo as $numero){
-            $resultado .= $numero."  ";
-        }
-    return $resultado;
-}
+print_r($arreglo);
     
 //Recorre y muestra cada elemento del arreglo
-echo "Lista Original: ".mostrarArreglo(print_r($arreglo));
+
 
 
 
