66b7e3d6db5edc1e92abc1aaec5a3562f3e0c01c
manriqueL <manriquelucasp@gmail.com>
2023-05-23 12:33:31 -0300

    Se modifica la forma de mostrar el array cargado con los 120 elementos

--- ejercicio2.php
+++ ejercicio2.php
@@ -1,16 +1,14 @@
 <?php
-
+//Cargar array con numeros desde 0 al 100, hasta que llegue a un maximo de 120 elementos.
 $arreglo = [];
 
 while (sizeof($arreglo) < 120) {
-    array_push($arreglo, random_int(0,1));
+    array_push($arreglo, random_int(0,100));
     
 };
 
 print_r($arreglo);
     
-//Recorre y muestra cada elemento del arreglo
-
 
 
 
