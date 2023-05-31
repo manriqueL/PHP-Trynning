8246f980e0d6cf7b84f9b441cc7f648f3f562f60
manriqueL <manriquelucasp@gmail.com>
2023-05-31 13:05:41 -0300

    Se agrega ejercicio 4 a carpeta ejercicios-2(sin completar

---
+++ ejercicios-2/ejercicio4.php
@@ -0,0 +1,18 @@
+<?php
+
+$numero1 = $_GET["numero1"];
+$numero2 = $_GET["numero2"];
+
+if (isset($numero1) && isset($numero2)) {   
+    if($numero1<$numero2){
+        for($i=$numero1; $i <= $numero2 ; $i++) { 
+            if($i % 2 == 1){    
+                echo "<h1>$i</h1>";
+            }
+        }    
+    }    
+}else{
+    echo "<h1>Ingrese los datos por URL</h1>";
+}
+
+?>
